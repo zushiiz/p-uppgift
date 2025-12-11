14743d3e0f7cfe97782e84d6b51dea71aaba6106
Zixuan Huang <zxgamingh@gmail.com>
2025-12-11 14:45:45 +0100

    encounter.py, player.py, attack input handling

--- encounter.py
+++ encounter.py
@@ -5,6 +5,7 @@ class Encounter:
     def __init__(self, player, opponent):
 
         self.player = player
+        self.playerPokemon = self.player.activePokemon
         self.opponent = opponent
         self.turn = 1
         self.stop = False
@@ -13,6 +14,27 @@ class Encounter:
         while self.stop != False:
             print("Encounter start")
             print(f"You've encountered a wild {self.opponent}!")
-            print(f"{self.player} sent out {self.player.activePokemon}")
-            self.player.getActionInput()
+            print(f"{self.player} sent out {self.playerPokemon}")
+            self.getActionInput()
+
+    def getActionInput(self):
+        validInput = False
+        while validInput == False:
+            print("[0] Fight \n" \
+                  "[1] Pokemon \n" \
+                  "[2] Items \n" \
+                  "[3] Run")
+            
+            userInput = input("What would you like to do?:")
+            match userInput:
+                case "0":
+                    self.player.attackOption()
+                case "1":
+                    self.player.swapAction()
+                case "2":
+                    self.player.itemsAction()
+                case "3":
+                    self.player.runAction()
+                case _:
+                    pass
 
--- player.py
+++ player.py
@@ -2,7 +2,7 @@ class Player:
     def __init__(self, username = "Guest", team = ["Empty"]):
         self.name = username
         self.team = team
-        self.firstSlot = team[0]
+        self.activePokemon = team[0]
 
         self.potions = 10
         healingAmount = 100
@@ -13,21 +13,26 @@ class Player:
     def __str__(self):
         return (f"User: {self.name}")
     
-    def playerInput(self, input):
-        match input:
-            case "0":
-                self.attackOption()
-            case "1":
-                self.swapOption()
-            case "2":
-                self.itemsOption()
-            case "3":
-                self.runOption()
-            case _:
+    def attackOption(self):
+        validInput = False
+        while validInput == False:
+            amountOfAttacks = []
+            for i in range(len(self.activePokemon.attacks)):
+                print(f"[{i}] {self.activePokemon.attacks[i]}")
+                amountOfAttacks.append(i)
+                i += 1
+            print(f"[4] Back")
+
+            userInput = input("What would you like to do?:")
+            if userInput == "4":
+                break
+            try:
+                if int(userInput) in amountOfAttacks:
+                    return int(userInput)
+                else:
+                    pass
+            except:
                 pass
-    
-    def attackOption():
-        return None
     
     def swapOption():
         return None    
