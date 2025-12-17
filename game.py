c0ea4cdc945de0051c99000a3a271790c57ac5ff
Zixuan Huang <zxgamingh@gmail.com>
2025-12-17 11:24:14 +0100

    game.py - implemented new dual import system to avoid copying class instance of pokemon (would need to read file alot, is it issue?)

--- game.py
+++ game.py
@@ -6,22 +6,40 @@ from player import Player
 from encounter import Encounter
 import csv, random, copy
 
-def importPokemon(fileName):
+# def importPokemon(fileName):
+#     pokemonList = []
+#     with open(fileName, "r", encoding="utf-8") as csvFile:
+#         reader = csv.DictReader(csvFile)
+#         for object in reader:
+#             stats = Stats(object["Health"], object["Attack"], object["Defense"], object["Speed"])
+#             level = Leveling(object["Level"], object["Can_evolve"], object["Stage"])
+#             pokemonList.append(Pokemon(object["Pokemon_name"], stats, MoveList(Attack("Scratch")), level, object["Next_evolution"]))
+#     return pokemonList
+
+def importPokemonNames(fileName):
     pokemonList = []
     with open(fileName, "r", encoding="utf-8") as csvFile:
         reader = csv.DictReader(csvFile)
         for object in reader:
-            stats = Stats(object["Health"], object["Attack"], object["Defense"], object["Speed"])
-            level = Leveling(object["Level"], object["Can_evolve"], object["Stage"])
-            pokemonList.append(Pokemon(object["Pokemon_name"], stats, MoveList(Attack("Scratch")), level, object["Next_evolution"]))
+            pokemonList.append(object["Pokemon_name"])
     return pokemonList
 
-def main():
-    l = importPokemon("data.txt")
+def importPokemonByName(fileName, pokemonName):
+    with open(fileName, "r", encoding="utf-8") as csvFile:
+        reader = csv.DictReader(csvFile)
+        for object in reader:
+            if object["Pokemon_name"] == pokemonName:
+                stats = Stats(object["Health"], object["Attack"], object["Defense"], object["Speed"])
+                level = Leveling(object["Level"], object["Can_evolve"], object["Stage"])
+                return Pokemon(object["Pokemon_name"], stats, MoveList(Attack("Scratch")), level, object["Next_evolution"])
 
-    l2 = copy.deepcopy(l)
-    b1 = l2[0]
-    b2 = l[0]
+def main():
+    l = importPokemonNames("data.txt")
+    for e in l:
+        print(e)
+
+    p = importPokemonByName("data.txt", l[3])
+    print(p)
     # playerTeam = [b1, l[3], b2]
     # enemy = l[4]
     # player = Player("Me", playerTeam)
@@ -29,10 +47,6 @@ def main():
     # encounter = Encounter(player, enemy)
     # encounter.startEncounter()
     # print("Encounter stopped")
-
-    b1.fainted = True
-    print(b1.fainted)
-    print(b2.fainted)
 
 
 main()
