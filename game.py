ee363447ab88df9b0fada825f975e7d6929d0fa7
Zixuan Huang <zxgamingh@gmail.com>
2026-01-13 10:56:56 +0100

    game.py, class-formatting instead of functions, methods added: mainGameLoop(), startMenu(), functions added: importPlayerTeam NEXT: Need to manage encounter end level system

--- game.py
+++ game.py
@@ -13,9 +13,9 @@ class MainGame:
         self.masterList = importPokemonNames(self.file) # All pokemon names
 
         playerTeam = [importPokemonByName(self.file, "Bulbasaur"), importPokemonByName(self.file, "Charmander"), importPokemonByName(self.file, "Bulbasaur")]
-        self.player = Player("Me", playerTeam)   
-
-        self.enemy = None
+        self.player = None
+        self.map = Map()
+        self.run = False
          
     
     def generateRandomEnemy(self):
@@ -23,10 +23,18 @@ class MainGame:
         for pokemon in self.player.team:
             playerLevels.append(pokemon.leveling.lvl)
         playerLevels.sort()
-        level = random.randint(playerLevels[0], len(playerLevels)-1)
+        level = random.randint(playerLevels[0], playerLevels[len(playerLevels)-1])
         nameIndex = random.randint(0, len(self.masterList)-1)
-        self.enemy = importPokemonByName(self.file, self.masterList[nameIndex], level)
-    
+        enemy = importPokemonByName(self.file, self.masterList[nameIndex], level)
+        return enemy
+
+    def mainGameLoop(self):
+        while self.run:
+            self.map.userInterface()
+            enemy = self.generateRandomEnemy()
+            encounterInstance = Encounter(self.player, enemy)
+            encounterInstance.startEncounter()
+
     def startMenu(self):
         while True:
             print("Welcome!")
@@ -37,17 +45,53 @@ class MainGame:
                 userInput = int(input("What would you like to do?:"))
                 match userInput:
                     case 0:
+                        username = input("Input username:")
+                        print("Pick your starter!\n"\
+                              "[0] Bulbasaur\n"\
+                              "[1] Squirtle\n"\
+                              "[2] Charmander\n") # No way back, might change later
+                        
+                        playerStarter = input(":")
+                        playerTeam = []
+                        match playerStarter:
+                            case "0":
+                                playerTeam.append(importPokemonByName(self.file, "Bulbasaur"))
+                                self.player = Player(username, playerTeam)
+                            case "1":
+                                playerTeam.append(importPokemonByName(self.file, "Squirtle"))
+                                self.player = Player(username, playerTeam)
+                            case "2":
+                                playerTeam.append(importPokemonByName(self.file, "Charmander"))
+                                self.player = Player(username, playerTeam)
+                        self.run = True
                         break
+
                     case 1:
-                        #import a team
+                        userFile = input("OBS! It has to be a .txt file with correct csv Format and in the correct directory!\n" \
+                        "Input the full filename: ")
+                        team = importPlayerTeam(userFile)
+                        username = input("Input username:")
+                        self.player = Player(username, userFile)
+                        self.run = True
                         break
+                    case 2:
+                        break
+            
             except ValueError:
                 print("Enter int") #Change later
                 continue
+        self.mainGameLoop()
 
-        
+def importPlayerTeam(fileName):
+    playerTeam = []
+    with open(fileName, "r", encoding="utf-8") as csvFile:
+        reader = csv.DictReader(csvFile)
+        for object in reader:
 
-
+            stats = Stats(object["Health"], object["Attack"], object["Defense"], object["Speed"])
+            level = Leveling(level, object["Can_evolve"], object["Stage"])
+            playerTeam.append(Pokemon(object["Pokemon_name"], stats, MoveList(Attack("Scratch")), level, object["Next_evolution"]))
+    return playerTeam
 
 # def importPokemon(fileName):
 #     pokemonList = []
@@ -108,7 +152,7 @@ def main():
     # encounter.startEncounter()
     # print("Encounter stopped")
     game = MainGame("data.txt")
-    game.generateRandomPokemon()
+    game.startMenu()
 
 
 main()
