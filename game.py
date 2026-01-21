100efe82b7e83ddda5c368d1415c1d044d07066f
Zixuan Huang <zxgamingh@gmail.com>
2026-01-21 23:11:14 +0100

    game.py - map now works, working on starting encounters, added some comments

--- game.py
+++ game.py
@@ -32,56 +32,51 @@ class MainGame:
         self.run = False
         self.gui = GUI()
 
-    def generateRandomEnemy(self):
+    def generateRandomEnemy(self): # Generates one random pokemon object for an encounter
+        """
+        Parameters: None
+        Return values: enemy=Pokemon()
+        """
         playerLevels = []
         for pokemonObj in self.player.team:
             playerLevels.append(pokemonObj.leveling.lvl)
-        playerLevels.sort()
+        playerLevels.sort() # Level balancing
         level = random.randint(playerLevels[0], playerLevels[len(playerLevels)-1])
         nameIndex = random.randint(0, len(self.masterList)-1)
         enemy = importPokemonByName(self.file, self.masterList[nameIndex], level)
         return enemy
 
-    def mainGameLoop(self):
-        while self.run:
-            menuActionIndex = self.map.userInterface()
-            match menuActionIndex:
-                case 0:
-                    self.player.changeActivePokemon()
-                case 1:
-                    self.run = exportPlayerTeam(self.player.team)
-                case _:
-                    enemy = self.generateRandomEnemy()
-                    encounterInstance = Encounter(self.player, enemy)
-                    encounterInstance.startEncounter()
-                    print(encounterInstance.playerWin)
-                    if encounterInstance.playerWin == True:
-                        expGained = encounterInstance.opponent.leveling.droppedExp
-                        nextEvolutions = getEvolutionName(self.masterList, self.player.activePokemon, self.file)
-                        self.player.activePokemon.gainExp(expGained, nextEvolutions)
-                        print(self.player.activePokemon.leveling)
-                    else:
-                        continue
+    def start(self): # Program starting point
+        """
+        Parameters: None
+        Return values: None
+        """        
+        self.gui.write_line("Enter a username")
+        self.gui.create_input_field()
+        self.gui.action_button.config(command = lambda: self._storeUsername(self.gui.input_field.get()))
 
-    def startMenu(self): # Could split into more functions/methods, also there is no back option from case 0 and 1
-
-        print("Welcome!")
-
+    def startMenu(self): # Initiates/Creates starting menu
+        """
+        Parameters: None
+        Return values: None
+        """
+        print("Starting Menu...")
         self.gui.write_line(f"Welcome {self.username}!")
-
         self.gui.create_actions_box()
         options = ["New Game", "Load Game", "Quit Game"]
         self.gui.update_listbox(options)
         self.gui.action_button.config(command = lambda:self.startMenuActions(self.gui.actions_box))        
-        
-        self.mainGameLoop()
 
-    def startMenuActions(self, list_box):     
+    def startMenuActions(self, list_box): # Handles the logic for if user wants to load or create new game
+        """
+        Parameters: list_box=tk.Listbox
+        Return values: None
+        """
         userInput = list_box.curselection()
         userInput = int(userInput[0])
-        print(userInput)
+        print(f"User input: {userInput}")
         match userInput:
-            case 0:
+            case 0: # Prepares menu and parameters for newGameMenu() method
                 self.gui.write_line("Pick your starter Pokemon!")
                 options = ["Bulbasaur", "Squirtle", "Charmander", "Back"]
                 self.gui.create_actions_box()
@@ -89,13 +84,13 @@ class MainGame:
                 self.gui.action_button.config(command = lambda:self.newGameMenu(self.gui.actions_box))
 
             case 1:
-                # team = importPlayerTeam()
-                # username = input("Input username:")
-                # self.player = Player(username, team)
-                # self.run = True
                 self.loadGameMenu()
 
-    def newGameMenu(self, list_box):
+    def newGameMenu(self, list_box): # Menu that lets user choose starter pokemon
+        """
+        Parameters: list_box=tk.Listbox
+        Return values: None
+        """        
         userInput = int(list_box.curselection()[0])
         print("New Game")
         playerTeam = []
@@ -113,30 +108,42 @@ class MainGame:
                 playerTeam.append(importPokemonByName(self.file, "Charmander"))
                 self.player = Player(username, playerTeam)
                 self.run = True
-            case 3:
+            case _:
                 self.gui.clear_action_frame()
                 self.startMenu()
-        self.mainGameLoop()
 
-    def loadGameMenu(self):
+        self.mapGui()
+
+    def loadGameMenu(self): # Menu that lets user import own file with correct format
+        """
+        Parameters: None
+        Return values: None
+        """                
         self.gui.write_line("OBS! It has to be a file with correct csv Format and in the correct directory!\n" \
                             "Input the FULL filename/path in the field")
         self.gui.create_input_field()
         self.gui.action_button.config(command = lambda: self.importPlayerTeam(self.gui.input_field.get()))
-
-    def start(self):
-        self.gui.write_line("Enter a username")
-        self.gui.create_input_field()
-        self.gui.action_button.config(command = lambda: self._storeUsername(self.gui.input_field.get()))
     
-    def _storeUsername(self, input):
-        self.username = input
-        self.startMenu()
-        print(self.username)
+    def _storeUsername(self, input): # Updates the username attribute
+        """
+        Parameters: input=string
+        Return values: None
+        """
+        if input == "":
+            self.username = "Guest"
+            print("Guest user")
+        else:
+            self.username = input
+            print(f"updated - self.username={self.username}")
+        self.startMenu()      
 
-    def importPlayerTeam(self, userFile):
+    def importPlayerTeam(self, userFile): # Imports the file that user enters
+        """
+        Parameters: userFile=string(file path)
+        Return values: None 
+        """        
         playerTeam = []
-        print(userFile)
+        print(f"Importing file:{userFile}")
         try:
             with open(userFile, "r", encoding="utf-8") as csvFile:
                 reader = csv.DictReader(csvFile)
@@ -145,13 +152,57 @@ class MainGame:
                     level = Leveling(pokemonData["Level"], pokemonData["Can_evolve"], pokemonData["Stage"])
                     playerTeam.append(Pokemon(pokemonData["Pokemon_name"], stats, MoveList(Attack("Scratch")), level, pokemonData["Next_evolution"]))
             self.player = Player(self.username, playerTeam)
+            print("Import successful, running game")
             self.run = True
-            self.mainGameLoop()
+            self.mapGui()
         except FileNotFoundError:
-            print("File not found")
+            print("Import failed\nError: File not found")
 
+    def mapGui(self): # Creates/Formats the gui for the map and dpad
+        """
+        Parameters: None
+        Return values: None
+        """
+        if self.run == True:
+            print("Run successful")
+            self.gui.disable_terminal()
+            self.gui.show_map(self.map)
+            self.gui.clear_action_frame()
+            self.gui.write_line("Started game! Use w/a/s/d to walk around!")
+    
+            options = ["Team", "Quit"]
+            self.gui.update_listbox(options)
+
+            self.gui.create_dpad()
+            self.gui.up.config(command = lambda: (self.map.movePlayer(0, -1), self.gui.refresh_map(self.map)))
+            self.gui.down.config(command = lambda: (self.map.movePlayer(0, 1), self.gui.refresh_map(self.map)))
+            self.gui.left.config(command = lambda: (self.map.movePlayer(-1, 0), self.gui.refresh_map(self.map)))
+            self.gui.right.config(command = lambda: (self.map.movePlayer(1, 0), self.gui.refresh_map(self.map)))
+
+            # menuActionIndex = self.map.userInterface()
+            # match menuActionIndex:
+            #     case 0:
+            #         self.player.changeActivePokemon()
+            #     case 1:
+            #         self.run = exportPlayerTeam(self.player.team)
+            #     case _:
+            #         enemy = self.generateRandomEnemy()
+            #         encounterInstance = Encounter(self.player, enemy)
+            #         encounterInstance.startEncounter()
+            #         print(encounterInstance.playerWin)
+            #         if encounterInstance.playerWin == True:
+            #             expGained = encounterInstance.opponent.leveling.droppedExp
+            #             nextEvolutions = getEvolutionName(self.masterList, self.player.activePokemon, self.file)
+            #             self.player.activePokemon.gainExp(expGained, nextEvolutions)
+            #             print(self.player.activePokemon.leveling)
+            #         else:
+            #             continue
 
 def exportPlayerTeam(playerTeam):
+    """
+    Parameters: playerTeam=[Pokemon(), ...]
+    Return values: Boolean
+    """
     while True:
         print("[2] Back")
         userFile = input("Avoid special characters such as '. , ? !' or numbers '1 2 3 4'\n"\
