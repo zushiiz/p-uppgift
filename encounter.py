9798013a967764dc279595606dd13e86a947bc79
Zixuan Huang <zxgamingh@gmail.com>
2026-01-24 11:52:40 +0100

    encounter.py - reworked swapping logic, both manual and pokemon fainting

--- encounter.py
+++ encounter.py
@@ -45,6 +45,12 @@ class Encounter:
         self.gui.update_listbox(options)
         self.gui.action_button.config(command = lambda:self.playerAction(self.gui.actions_box))
 
+    def backToEncounterMenu(self):
+        self.gui.create_actions_box()
+        options = ["Fight", "Pokemon", "Items", "Run"]
+        self.gui.update_listbox(options)
+        self.gui.action_button.config(command = lambda:self.playerAction(self.gui.actions_box))
+
     def playerAction(self, listbox): 
         userInput = listbox.curselection()
 
@@ -64,28 +70,41 @@ class Encounter:
                     options.append(e)
                 options.append("Back")
                 self.gui.update_listbox(options)
-                self.gui.action_button.config(command = lambda:self.playerFightMenu(self.gui.actions_box))
+                self.gui.action_button.config(command = lambda:self.playerFightMenu(self.gui.actions_box, options))
                 
             case 1:
-                pass
-                # Switch
+                self.gui.create_actions_box()
+                options = []
+                for e in self.player.team:
+                    if e == self.playerPokemon:
+                        continue
+                    else:
+                        options.append(f"{e.name} - {e.stats.hp}/{e.stats.maxHp}")
+                options.append("Back")
+                self.gui.update_listbox(options)
+                self.gui.action_button.config(command = lambda:self.playerSwapMenu(self.gui.actions_box, options))
+
             case 2:
                 # Items
                 pass
             case 3:
-                # Run
-                pass
+                self.gui.write_line("You've fled from the encounter")
+                self.stopEncounter()
             case _: # Should never run, but just in case
                 print(f"Invalid action index: {userInput}")
 
-    def playerFightMenu(self, listbox): # Finish
+    def playerFightMenu(self, listbox, options): # Finish
         userInput = listbox.curselection()
-        
+
         if len(userInput) == 0:
             raise ValueError("No actions selected")
+
+        if options[int(userInput[0])] == "Back":
+            self.backToEncounterMenu()
+            return
         
         userInput = userInput[0]
-        print(f"User input - fightMenu(): {userInput}")
+        print(f"User input - playerFightMenu(): {userInput}")
 
         enemyAttack = random.randint(0, (len(self.opponent.attacks)-1))
         faintedObject = None # Redundant
@@ -100,53 +119,87 @@ class Encounter:
             faintedObject, fainted = self.fight(enemyAttack, userInput, self.opponent, self.playerPokemon)                     
 
         # Post damage logic
-        if faintedObject == self.opponent and fainted == True:
-            self.stopEncounter()
+        if faintedObject == self.opponent and fainted == True: # Opponent loss
             print(f"{faintedObject} fainted. Stats: {faintedObject.stats}")
             self.playerWin = True
-        elif faintedObject == self.playerPokemon and fainted == True:
+            self.stopEncounter()
+
+        elif faintedObject == self.playerPokemon and fainted == True: # Player loss
             if self.checkPlayerStatus():
                 print("Player defeat. Stopping encounter")
 
                 self.gui.write_line(f"All {self.player}'s pokemon have died")
                 self.stopEncounter()
                 
-            else: # Check this logic
-                self.playerPokemon = self.player.swapOption(back = False)
-                self.gui.write
+            else: # I think this has some logic errors?
+                # If player has more pokemons
+                self.gui.write_line(f"{self.playerPokemon} has fainted!\n"\
+                                    "Who would you like to send out?")
+                self.gui.create_actions_box()
+                options = []
+                for e in self.player.team:
+                    options.append(f"{e.name} - {e.stats.hp}/{e.stats.maxHp}")
+                self.gui.update_listbox(options)
+                self.gui.action_button.config(command = lambda:self.playerSwapMenu(self.gui.actions_box, options, faint = True))
+                
                 print(f"{self.player} sent out {self.playerPokemon}")
         else:
-            self.nextTurn()   
+            self.nextTurn()
         
-        #         case "1":
-        #             playerActionIndex = self.player.swapOption()
-        #             if playerActionIndex == 6:
-        #                 continue
-        #             else: # check logic for pokemon if it dies instantly, this loops needs to rerun
-        #                 self.playerPokemon = playerActionIndex
-        #                 self.enemyAttack()
-                
-        #         case "2":
-        #             playerActionIndex = self.player.itemsOption()
-        #             if playerActionIndex == 1:
-        #                 if self.catchPokemon():
-        #                     self.stopEncounter()
-        #                     break
-        #                 else:
-        #                     self.enemyAttack()
-        #             elif playerActionIndex == 2:
-        #                 continue
-        #             else:
-        #                 self.enemyAttack()
+    def playerSwapMenu(self, listbox, options, faint = False):
+        userInput = listbox.curselection()
+        
+        if len(userInput) == 0:
+            raise ValueError("No actions selected")
+        
+        if options[int(userInput[0])] == "Back":
+            self.backToEncounterMenu()
+            return
+        
+        userInput = userInput[0]
+        print(f"User input - playerSwapMenu(): {userInput}")
+        
+        if self.player.team[userInput].fainted == True:
+            self.gui.write_line("Cannot send out fainted pokemon")
+        
+        else:
+            self.player.swapOption(userInput)
+            self.playerPokemon = self.player.activePokemon
+            self.gui.write_line(f"{self.player.name} sent out {self.playerPokemon}")
+            if not faint:
+                self.enemyAttack()
+            self.nextTurn()
 
-        #         case "3":
-        #             self.stop = True
-        #             break
-                
-        #         case _:
-        #             continue          
-            
-        #     self.nextTurn()
+
+#         case "1":
+#             playerActionIndex = self.player.swapOption()
+#             if playerActionIndex == 6:
+#                 continue
+#             else: # check logic for pokemon if it dies instantly, this loops needs to rerun
+#                 self.playerPokemon = playerActionIndex
+#                 self.enemyAttack()
+        
+#         case "2":
+#             playerActionIndex = self.player.itemsOption()
+#             if playerActionIndex == 1:
+#                 if self.catchPokemon():
+#                     self.stopEncounter()
+#                     break
+#                 else:
+#                     self.enemyAttack()
+#             elif playerActionIndex == 2:
+#                 continue
+#             else:
+#                 self.enemyAttack()
+
+#         case "3":
+#             self.stop = True
+#             break
+        
+#         case _:
+#             continue          
+    
+#     self.nextTurn()
 
     def checkPlayerStatus(self): # True if all fainted 
         amount = 0
@@ -191,15 +244,17 @@ class Encounter:
         print(f"{self.player} used Pokeball!")
         if random.randint(1, 4) == 1:
             print(f"{self.opponent} was caught!")
-            self.player.team.append(self.opponent) # Måste lägga cap
+            self.player.team.append(self.opponent) # No limit
             return True
         else:
             print(f"{self.opponent} escaped!")
             return False
 
-    def enemyAttack(self):
+    def enemyAttack(self): # When only enemy attacks
         enemyAttack = random.randint(0, (len(self.opponent.attacks)-1))
+        self.gui.write_line(f"{self.opponent.name} used {self.opponent.attacks[enemyAttack]}")
         self.opponent.attack(self.playerPokemon, enemyAttack)
+        self.gui.write_line(f"{self.playerPokemon.name} - Hp: {self.playerPokemon.stats.hp}/{self.playerPokemon.stats.maxHp}")
         print(f"{self.opponent} used {self.opponent.attacks[enemyAttack]}")
         print(self.playerPokemon.stats)
         print(self.opponent.stats)
